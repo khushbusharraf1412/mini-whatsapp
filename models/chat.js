8f02890e92ae3cd8f68292b0d5f50bc1a1f268d9
Khushbu Sharraf <dancer.khushbu14@gmail.com>
2025-04-28 18:16:05 +0530

    Add files via upload

--- models/chat.js
+++ models/chat.js
@@ -1,1 +1,22 @@
+const mongoose=require("mongoose");
 
+const chatschema=new mongoose.Schema({
+    from:{
+        type:String,
+        required:true,
+    },
+    to:{
+    type:String,
+    required:true,
+    },
+    msg:{
+        type:String,
+        maxLength:50,
+    },
+    created_at:{
+        type:String,
+        required:true,
+    }
+});
+const chat=mongoose.model("chat",chatschema);
+module.exports=chat;
